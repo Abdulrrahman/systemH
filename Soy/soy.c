c4d1fc5de78e736fb78b0df36e0f20f3d52b626a
MR.Joukhadar <A.joukhadar@hotmail.com>
2017-03-11 20:04:20 +0300

    creat folder and file then write inside it
    run ./soy + anystring uwant as family same
    then the program will create folder with that name and it will make a main.txt file inside it and it has the diroctroy to that file inside the txt file

--- Soy/soy.c
+++ Soy/soy.c
@@ -6,7 +6,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define falloc(ty, sz) (ty *) malloc (sz * sizeof(ty))
+#define falloc(ty, sz) (ty *) malloc (sz * sizeof(ty))//creat anything
+//to connect 2 charts to eachother created by me :D Im genius
+#define conch(str1,str2) ((char *) strcat(strcpy(falloc(char,1 + strlen(str1)+ strlen(str2)),str1), str2))
 
 typedef struct String{
     char *text;
@@ -16,26 +18,43 @@ int main(argc, argv)
 int argc;
 char **argv;
 {
-  if (argc != 2) {
+  char * mode="w+" ;                    //mode to write and read for .txt files
+  struct stat st = {0};                 //to check the address of folder we made
+
+  if (argc != 2) {                      //error checking for input of program name and the name of family-tree
     fprintf(stderr, "usage: ./soy familyname\n");
     exit(1);
   }
-  string a = falloc(struct String, 2);
-  a[0].text = "mother";
-  a[1].text = "w";
-  
-  struct stat st = {0};
 
-  if (stat(argv[1], &st) == -1) {
+  /*string a = falloc(struct String, 2);
+  a[0].text = "mother";
+  a[1].text = "w";*/
+  
+  if (stat(argv[1], &st) == -1) {       //if the folder is not exists creat else do not
     mkdir(argv[1], 0755);
   }
-  const char * filename = "./adam/sample.txt";
-  const char * mode="w+" ;
-  if (stat(filename, &st) != -1) {
+  else
+  {
     printf("error the file already exists\n");
   }
-  FILE* fp= fopen(filename, mode);
 
+  char * filename = conch("./",argv[1]);// look up in define explanation is there
+  filename = conch(filename,"/main.txt");//after this comment we have the diroctory of the file like ./familyname/main.txt
+  
+  if (stat(filename, &st) != -1) {  //is the file exists ?
+    printf("error the file already exists\n");
+  }
+  else
+  {
+    FILE* fp= fopen(filename, mode);//if not creat one with the filename diroctory
+    if (fp == NULL)//if the file didn`t open
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(fp, "file id: %s\n", filename);//write the filename as file id to the .txt file
+  }
+  printf("%s\n",filename);//that wat we printed to the file
   exit(0);
   return 0;
 }
